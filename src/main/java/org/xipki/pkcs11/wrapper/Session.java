fdb6e86edf185f51754d172652a0b5a24bbc0cbd
Lijun Liao <lijun.liao@gmail.com>
2023-02-09 07:28:26 +0100

    rename method createECPrivateKeyObject to createPrivateKeyObject

--- src/main/java/org/xipki/pkcs11/wrapper/Session.java
+++ src/main/java/org/xipki/pkcs11/wrapper/Session.java
@@ -14,7 +14,9 @@ import sun.security.pkcs11.wrapper.PKCS11;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.math.BigInteger;
+import java.security.PublicKey;
 import java.security.interfaces.ECPublicKey;
+import java.security.spec.ECPoint;
 import java.util.*;
 
 /**
@@ -285,13 +287,14 @@ public class Session {
     }
   }
 
-  public long createECPrivateKeyObject(AttributeVector template, ECPublicKey publicKey) throws PKCS11Exception {
-    if (publicKey != null && privateKeyWithEcPoint(template.keyType())) {
+  public long createPrivateKeyObject(AttributeVector template, PublicKey publicKey) throws PKCS11Exception {
+    if (publicKey instanceof ECPublicKey && privateKeyWithEcPoint(template.keyType())) {
       byte[] ecParams = template.ecParams();
       Integer fieldSize = Functions.getECFieldSize(ecParams);
+      ECPoint w = ((ECPublicKey) publicKey).getW();
 
-      byte[] wx = Functions.asUnsignedByteArray(publicKey.getW().getAffineX());
-      byte[] wy = Functions.asUnsignedByteArray(publicKey.getW().getAffineY());
+      byte[] wx = Functions.asUnsignedByteArray(w.getAffineX());
+      byte[] wy = Functions.asUnsignedByteArray(w.getAffineY());
       if (fieldSize == null) {
         fieldSize = Math.max(wx.length, wy.length);
       } else {
