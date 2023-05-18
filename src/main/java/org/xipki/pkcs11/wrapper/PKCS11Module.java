0855173c6dc12fd191d9145e269f22cd29283790
Lijun Liao <lijun.liao@gmail.com>
2023-05-18 22:29:04 +0200

    add vendor behavior to ignore non-zero ulDeviceError

--- src/main/java/org/xipki/pkcs11/wrapper/PKCS11Module.java
+++ src/main/java/org/xipki/pkcs11/wrapper/PKCS11Module.java
@@ -90,6 +90,11 @@ public class PKCS11Module {
   static final int BEHAVIOUR_SM2_PRIVATEKEY_ECPOINT = 4;
 
   /**
+   * Ignore the non-zero ulDeviceError in the SessionInfo.
+   */
+  static final int BEHAVIOUR_IGNORE_DEVICE_ERROR = 5;
+
+  /**
    * Interface to the underlying PKCS#11 module.
    */
   private PKCS11 pkcs11;
@@ -421,6 +426,8 @@ public class PKCS11Module {
                 vendorBehaviours.add(BEHAVIOUR_SM2_PRIVATEKEY_ECPOINT);
               } else if ("EC_PRIVATEKEY_ECPOINT".equalsIgnoreCase(token)) {
                 vendorBehaviours.add(BEHAVIOUR_EC_PRIVATEKEY_ECPOINT);
+              } else if ("IGNORE_DEVICE_ERROR".equalsIgnoreCase(token)) {
+                vendorBehaviours.add(BEHAVIOUR_IGNORE_DEVICE_ERROR);
               } else {
                 StaticLogger.warn("Ignored unknown vendor behaviour '" + token + "'.");
               }
@@ -497,7 +504,7 @@ public class PKCS11Module {
           } else if (name.equalsIgnoreCase("module.version")) {
             block.versions = textList;
           }
-        } else if (line.startsWith("CKD_") || line.startsWith("CKG_") ||
+        } else if (line.startsWith("CKD_") || line.startsWith("CKG_") || line.startsWith("CKU_") ||
             line.startsWith("CKK_") || line.startsWith("CKM_") || line.startsWith("CKR_")) {
           int idx = line.indexOf(' ');
           if (idx != -1) {
